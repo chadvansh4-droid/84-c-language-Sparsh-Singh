870cb0afdca318f657a5836d46bc3956b6d8ee37
Sparsh Singh <chadvansh4@gmail.com>
2026-09-17 16:08:23 +0530

    Create unsigned.c

---
+++ lab_04/unsigned.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int a;
+
+    printf("Enter a positive number: ");
+    scanf("%u", &a);
+
+    printf("Unsigned number = %u\n", a);
+
+    return 0;
+}
